fed00fb075efaedcbea1b9375458128588c8c7ce
Alexey Ulashchick <a.ulashckick@gmail.com>
2020-06-30 18:44:42 -0700

    HW01 - Usage of Guava Lists

--- hw01-gradle/src/main/java/ru/otus/Application.java
+++ hw01-gradle/src/main/java/ru/otus/Application.java
@@ -1,8 +1,16 @@
 package ru.otus;
 
+import com.google.common.collect.Lists;
+
 public class Application {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  private final static int DIFF = 'a' - 'A';
+
+  public static void main(String[] args) {
+    Lists.charactersOf("otus")
+        .stream()
+        .map(ch -> ch - DIFF)
+        .map(Character::toChars)
+        .forEach(System.out::print);
+  }
 }

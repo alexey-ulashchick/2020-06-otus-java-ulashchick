412d7f60b6942d7230df43f7ae1598052500f68f
Alexey Ulashchick <a.ulashckick@gmail.com>
2020-07-28 20:40:12 -0700

    HW02 - MyList & Basic testing

--- hw01-gradle/src/main/java/ru/otus/Application.java
+++ hw01-gradle/src/main/java/ru/otus/Application.java
@@ -1,16 +1,20 @@
 package ru.otus;
 
 import com.google.common.collect.Lists;
+import java.util.logging.Logger;
+
 
 public class Application {
 
-  private final static int DIFF = 'a' - 'A';
+  private static final int DIFF = 'a' - 'A';
+  private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
 
   public static void main(String[] args) {
     Lists.charactersOf("otus")
         .stream()
         .map(ch -> ch - DIFF)
         .map(Character::toChars)
-        .forEach(System.out::print);
+        .map(String::new)
+        .forEach(LOGGER::info);
   }
 }
